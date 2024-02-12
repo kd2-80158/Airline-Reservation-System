535739869bccf5686566df53d7b6afdea8f71a68
Madhur <Madhur>
2024-02-12 13:31:22 +0530

    Location entity added

---
+++ spring_boot_backend/src/main/java/com/app/entities/Location.java
@@ -0,0 +1,30 @@
+package com.app.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="location")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Location extends BaseEntity {
+	
+	@Column(length=30, nullable=false, unique=true)
+	private String city;
+	@Column(length=30, nullable=false, unique=true)
+	private String country;
+	@Column(length=10, nullable=false, unique=true)
+	private int airportCode;
+	
+	
+	
+}
